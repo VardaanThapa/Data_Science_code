b51976b903866c3fafbd1b45122448b6bf912a1b
Vardaan Thapa <65114618+VardaanThapa@users.noreply.github.com>
2021-01-03 12:54:49 +0530

    Add files via upload

---
+++ dplyr.R
@@ -0,0 +1,53 @@
+library(dplyr)
+
+## 
+library(nycflights13)
+
+
+a <- nycflights13::flights
+
+
+dim(nycflights13::flights)
+head(flights,10)
+flights<-flights
+
+## --------------
+filter(flights, month == 1, day == 1)
+
+## ----------
+arrange(flights, year, month, day)
+
+## -----------
+arrange(flights, desc(arr_delay))
+
+## ----------------
+# Select columns by name
+select(flights, year, month, day)
+# Select all columns between year and day (inclusive)
+select(flights, year:day)
+# Select all columns except those from year to day (inclusive)
+filtered_data<- select(flights, -(year:day))
+
+filtered_data
+## Create new columns-----------------
+data_new<-mutate(flights,
+  gain = arr_delay - dep_delay,
+  speed = distance / air_time * 60
+)
+
+## -------------
+data_new<-mutate(data_new,
+  gain = arr_delay - dep_delay,
+  gain_per_hour = gain / (air_time / 60)
+)
+
+## ---------------
+sample_n(flights, 10)
+sample_frac(flights, 0.01)
+
+
+
+
+
+
+
